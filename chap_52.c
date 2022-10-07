e32ef98af77ea5d8ecc0394ce229a432d4e38abe
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 17:47:08 +0530

    Convert meter to feet.c
    output:
    Enter Length in Meters  : 25
    25.000000 meter in feet = 81.500000

---
+++ chap_52.c
@@ -0,0 +1,9 @@
+int main()
+{
+    float meter;
+    float feet;
+    printf("\n\n Enter Length in Meters  : ");
+	scanf("%f", &meter);
+	feet = (meter*3.26);
+	printf("\n\n %f meter in feet = %f  \n",meter,feet);
+}
