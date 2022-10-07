e2f8a027bd99be1106db9da42f3e6b0238ee1519
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 22:56:55 +0530

    Perimeter of a square.c
    output:
    Enter the Side of the square: 4
    Perimeter of square is: 16.00

---
+++ chap_64.c
@@ -0,0 +1,10 @@
+int main()
+{
+  float side, perimeter, temp;
+  printf("Enter the Side of the square: ");
+  scanf("%f",&side);
+  temp =  4 ;
+  perimeter = temp * side  ;
+  printf("Perimeter of square is: %.2f",perimeter);
+  return 0;
+}
