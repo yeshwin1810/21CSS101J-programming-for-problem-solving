bf879fa29e0a46c17fde3cff8a907064a0d70d14
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 11:35:49 +0530

    Find ACII value
    output:
    Enter a character: y
    ASCII value of y = 121

--- chap_11.c
+++ chap_11.c
@@ -3,7 +3,7 @@ int main()
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);  
-    printf("ASCII value of %c = %d", c, c)
+    printf("ASCII value of %c = %d", c, c);
     
     return 0;
 }
