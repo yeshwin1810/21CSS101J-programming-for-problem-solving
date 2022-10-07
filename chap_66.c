009fbc995c920a1aa24ec74ece86032c592de2cf
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 23:03:15 +0530

    Square of a number.c
    output:
    Please Enter any integer Value : 5
    Square of a given number 5 is  =  25

---
+++ chap_66.c
@@ -0,0 +1,9 @@
+int main()
+{
+  int number, Square;
+  printf(" \n Please Enter any integer Value : ");
+  scanf("%d", &number);
+  Square = number * number;
+  printf("\n Square of a given number %d is  =  %d", number, Square);
+  return 0;
+}
