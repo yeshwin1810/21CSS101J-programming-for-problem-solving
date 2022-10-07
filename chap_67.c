ae6fcef4791458c87a6e04aaab14526bd52ced96
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 23:07:01 +0530

    Squareroot of a number.c
    output:
    Enter a number: 36
    Square root of 36.00 =  6.00

---
+++ chap_67.c
@@ -0,0 +1,9 @@
+int main()
+{
+   double number, squareRoot;
+   printf("Enter a number: ");
+   scanf("%lf", &number);
+   squareRoot = sqrt(number);
+   printf("Square root of %.2lf =  %.2lf", number, squareRoot);
+   return 0;
+}
