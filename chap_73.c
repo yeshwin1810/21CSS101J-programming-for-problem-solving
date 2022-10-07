f206a28954e30394aceb88e5f979c607b80274ae
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-07 23:32:54 +0530

    Fahrenheit to Celsius.c
    output:
    Enter Temperature Value (in Fahrenheit): 35
    Equivalent Temperature (in Celsius) = 1.67

---
+++ chap_73.c
@@ -0,0 +1,10 @@
+int main()
+{
+    float fahrenheit, celsius;
+    printf("Enter Temperature Value (in Fahrenheit): ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit-32)/1.8;
+    printf("\nEquivalent Temperature (in Celsius) = %0.2f", celsius);
+    getch();
+    return 0;
+}
