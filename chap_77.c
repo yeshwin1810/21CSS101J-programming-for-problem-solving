6b3071ee092922365ed20349b3479c8843512ea0
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-08 13:23:23 +0530

    Nested loop.c
    output:
    	
    				* * * * * 
    				* * * * * 
    				* * * * *

---
+++ chap_77.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int i,j,k;
+    for(i = 0; i < 5; i++)
+    {
+        printf("\t\t\t\t");
+        for(j = 0; j < 5; j++)
+        printf("* ");
+        printf("\n");
+    }
+    
+    return 0;
+}
