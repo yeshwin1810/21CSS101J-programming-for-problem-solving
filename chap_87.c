e3720ae1373fdf04669b5080776dc9f55dee8594
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-08 23:28:21 +0530

    Volume of cuboid.c
    output:
    enter length : 10
    enter breadth : 5
    enter height : 7
    VOC: 350.000000

---
+++ chap_87.c
@@ -0,0 +1,13 @@
+int main()
+{
+	float length,breadth,height,volume;
+	printf("enter length : ");
+	scanf("%f",&length);
+	printf("enter breadth : ");
+	scanf("%f",&breadth);
+	printf("enter height : ");
+	scanf("%f",&height);
+   	volume=length*breadth*height;
+	printf("VOC: %f\n",volume);
+	return 0;
+}
