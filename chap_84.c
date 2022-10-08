65c1521c05ce93e10e5ee7fc479ec6c4d09f31a7
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-08 23:20:04 +0530

    Volume of cube.c
    output:
    enter radius: 10
    enter height: 5
    VOC: 1571.428589

---
+++ chap_84.c
@@ -0,0 +1,11 @@
+int main()
+{
+    float vol,r,h;
+	printf("enter radius: ");
+	scanf("%f",&r);
+	printf("enter height: ");
+	scanf("%f",&h);
+	vol=(22*r*r*h)/7;
+	printf("VOC: %f\n",vol);
+	return 0;
+}
