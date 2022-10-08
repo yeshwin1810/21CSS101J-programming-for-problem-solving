9f52bf5a8dabd583e06a26a27a19ead913fe791d
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-08 23:26:25 +0530

    Volume of cone.c
    output:
    enter radius : 5
    enter height : 4
    VOC: 104.761902

---
+++ chap_86.c
@@ -0,0 +1,11 @@
+int main()
+{
+    float radius,height,volume;
+	printf("enter radius : ");
+	scanf("%f",&radius);
+	printf("enter height : ");
+	scanf("%f",&height);
+	volume=(22*radius*radius*height)/(3*7);
+	printf("VOC: %f\n",volume);
+	return 0;
+}
