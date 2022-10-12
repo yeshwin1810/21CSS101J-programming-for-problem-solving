89fc908fa1a6caf141a4f4b38ef0d6f3b3b34cb9
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-12 10:12:03 +0530

    Printing an integer.c
    output:
    RA2211042010009
    Enter an integer: 78
    You entered: 78

--- chap_08.c
+++ chap_08.c
@@ -1,10 +1,10 @@
 
 int main()
 {   
+    printf("RA2211042010009\n");
     int number;
     printf("Enter an integer: ");  
     scanf("%d", &number);
     printf("You entered: %d", number);
-    
     return 0;
 }
