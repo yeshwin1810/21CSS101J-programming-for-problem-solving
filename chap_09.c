6c229aefe603db29e95643910fa617b6369e549e
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-12 10:15:29 +0530

    Addition of three numbers.c
    output:
    RA2211042010009
    Enter three integers: 12 13 14
    12 + 13 +14 = 39

--- chap_09.c
+++ chap_09.c
@@ -1,10 +1,10 @@
 int main()
-{    
-
-    int number1, number2, sum;
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-    sum = number1 + number2;      
-    printf("%d + %d = %d", number1, number2, sum);
+{   
+    printf("RA2211042010009\n");
+    int number1, number2, number3, sum;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &number1, &number2, &number3);
+    sum = number1 + number2 + number3;      
+    printf("%d + %d +%d = %d", number1, number2, number3, sum);
     return 0;
 }
