c54b4a61f89f4dfa44063ba5bd832c894764e27e
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-12 10:19:36 +0530

    ASCII value.c
    output:
    RA2211042010009
    Enter a character: y
    ASCII value of y = 121

--- chap_11.c
+++ chap_11.c
@@ -1,9 +1,9 @@
 int main()
 {  
+    printf("RA2211042010009\n");
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);  
     printf("ASCII value of %c = %d", c, c);
-    
     return 0;
 }
