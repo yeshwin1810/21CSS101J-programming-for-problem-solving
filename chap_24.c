0bead3379a540cbd7356922b00cd50b328361899
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 19:01:04 +0530

    Check whether an Alphabet is Vowel or Consonant.c
    output:
    RA2211042010009
    Enter an alphabet: e
    e is a Vowel

--- chap_24.c
+++ chap_24.c
@@ -1,11 +1,10 @@
 int main()
 {
+    printf("RA2211042010009\n");
     char ch;
     bool isVowel = false;
-
     printf("Enter an alphabet: ");
     scanf("%c",&ch);
-
     if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'
     		||ch=='o'||ch=='O'||ch=='u'||ch=='U')
     {
