94004a1cee1bfd2885f1c902cad0fe577aa3fb15
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 19:05:45 +0530

    LCM of two numbers.c
    output:
    RA2211042010009
    Enter two positive integers: 12 3
    LCM of input numbers 12 and 3 is 12.

--- chap_26.c
+++ chap_26.c
@@ -1,12 +1,10 @@
 int main() 
 {
+  printf("RA2211042010009\n");
   int num1, num2, max;
   printf("Enter two positive integers: ");
   scanf("%d %d", &num1, &num2);
-
-  
   max = (num1 > num2) ? num1 : num2;
-
   while (1) {
     if (max % num1 == 0 && max % num2 == 0) {
       printf("LCM of input numbers %d and %d is %d.", num1, num2, max);
