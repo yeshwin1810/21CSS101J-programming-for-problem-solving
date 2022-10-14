ca907ae979f1fa1fda8fdb91a7dd6b7830f6a569
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 18:59:00 +0530

    chap_23.c
    output:
    RA2211042010009
    Enter year: 3045
    3045 is not a Leap Year

--- chap_23.c
+++ chap_23.c
@@ -1,14 +1,12 @@
 int main()
 {
     int y;
-
+    printf("RA2211042010009\n");
     printf("Enter year: ");
     scanf("%d",&y);
-
     if(y % 4 == 0)
     {
-    	//Nested if else
-        if( y % 100 == 0)
+    	if( y % 100 == 0)
         {
             if ( y % 400 == 0)
                 printf("%d is a Leap Year", y);
