df8d5a5a0e5ba4d959d10422bd1277593f385a57
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 20:52:54 +0530

    Area of semi circle.c
    output:
    RA2211042010009
    enter radius of the circle: 5
    Area Of Semi Circle: 39.000000

--- chap_61.c
+++ chap_61.c
@@ -1,10 +1,11 @@
 int main()
 {
+	printf("RA2211042010009\n");
 	int r;
 	float area;
 	printf("enter radius of the circle: ");
 	scanf("%d",&r);
 	area=(22*r*r)/(2*7);
-	printf("AOSC: %f\n",area);
+	printf("Area Of Semi Circle: %f\n",area);
 	return 0;
 }
