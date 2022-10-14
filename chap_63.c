40c0d761b3c5be2f1459d72f50a909965821fcfb
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 20:57:28 +0530

    volume of cone.c
    output:
    RA2211042010009
    enter side of cube: 3
    Volume Of Cone: 27.000000

--- chap_63.c
+++ chap_63.c
@@ -1,9 +1,10 @@
 int main()
 {
+	printf("RA2211042010009\n");
   float side,area;
 	printf("enter side of cube: ");
 	scanf("%f",&side);
   area=side*side*side;
-	printf("VOC: %f\n",area);
+	printf("Volume Of Cone: %f\n",area);
 	return 0;
 }
