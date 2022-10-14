ecd3a43a1db10e2a1752f3fbbe074cb135042500
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-10-14 20:11:41 +0530

    Float as input.c
    output:
    RA2211042010009
    Enter the number (float) : 45.2
    The number we got as input = 45.200001

---
+++ chap_42.c
@@ -0,0 +1,8 @@
+int main()
+{
+  printf("RA2211042010009\n");
+  float input_float;
+  printf("Enter the number (float) : ");
+  scanf("%f", &input_float);
+  printf("The number we got as input = %f",input_float);
+}
--- chap_48.c
+++
@@ -1,7 +0,0 @@
-int main()
-{
-float input_float;
-printf("Enter the number (float) : ");
-scanf("%f", &input_float);
-printf("The number we got as input = %f",input_float);
-}
