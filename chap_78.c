40196e6167bbe21b0eab8c2c8b498643ffd3e937
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 17:05:56 +0530

    arithmetic operators.c
    output:
    RA2211042010009
    Result of addition = 14.500000
    Result of subtraction = 10.500000
    result of multiplication = 25.000000
    result of division = 6.250000

---
+++ chap_78.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+void main() {
+    printf("RA2211042010009\n");
+    float num1= 12.5, num2= 2.0;
+    printf("Result of addition = %f\n",(num1 + num2));
+    printf("Result of subtraction = %f\n",(num1 - num2));
+    printf("result of multiplication = %f\n",(num1 * num2));
+    printf("result of division = %f\n",(num1 / num2));
+}
