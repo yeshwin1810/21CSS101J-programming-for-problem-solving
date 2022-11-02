5c5a4281233a38c5470c94cc1613c1501430ba10
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 16:12:32 +0530

    long int data type.c
    output:
    RA2211042010009
    given integers are num1=123456 and num2= 123456
    product of the give two numbers = 246912

---
+++ chap_71.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    long int num1= 123456L, num2= 123456L,result;
+    printf("given integers are num1=%d and num2= %d\n",num1, num2);
+    result = num1 + num2;
+    printf("product of the give two numbers = %d\n",result);
+}
