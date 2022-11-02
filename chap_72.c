b3179e51b33d210b673f36a2569647050ecd4106
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 16:19:35 +0530

    float data type.c
    output:
    RA2211042010009
    given integers are num1=5.340000 and num2= 125.789001
    the result after dividing in float format = 0.042452
    The result after dividing in exponential format = 4.245204e-02

---
+++ chap_72.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    float num1= 5.34, num2= 125.789f,result;
+    printf("given integers are num1=%f and num2= %f\n",num1, num2);
+    result = num1 / num2;
+    printf("the result after dividing in float format = %f\n",result);
+    printf("The result after dividing in exponential format = %e\n");
+}
