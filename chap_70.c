22857feb3f0b5d65065956350da603d0c1915bc5
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 16:08:32 +0530

    short int data type.c
    output:
    RA2211042010009
    given integers are num1=5 and num2= 7
    product of the give two numbers = 35

---
+++ chap_70.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    short int num1= 5, num2=7,result;
+    printf("given integers are num1=%d and num2= %d\n",num1, num2);
+    result = num1 * num2;
+    printf("product of the give two numbers = %d\n",result);
+}
