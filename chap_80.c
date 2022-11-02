61f27e8dc3c2207c74ecbb654fbb51ed7daa216e
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 17:33:48 +0530

    Relational and equality operators.c
    output:
    RA2211042010009
    result1 = 1
    result2 = 0
    result3 = 1
    result4 = 1
    result5 = 0

---
+++ chap_80.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+void main() {
+  printf("RA2211042010009\n");
+    int num1=7;
+    float num2=5.5;
+    char ch= 'w';
+    printf("result1 = %d\n",(num1>5));
+    printf("result2 = %d\n",((num1 + num2) <= 10));
+    printf("result3 = %d\n", (ch==119));
+    printf("result4 = %d\n",(ch != 'p'));
+    printf("result5 = %d\n",(ch >=10 * (num1+num2)));
+}
