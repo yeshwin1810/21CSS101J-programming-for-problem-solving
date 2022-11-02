006748098688dc13954188f831637ff9ab368254
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 15:44:35 +0530

    int data type from binary, octal, hex and character literals.
    output:
    RA2211042010009
    binaryThree-value = 3
    octalEight-value = 8
    hexTen-value = 10
    asciiValueOfOne-value 49
    asciiValueOfA-value = 65

---
+++ chap_68.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+  printf("RA2211042010009\n");
+  int binaryThree = 0b11;
+  printf("binaryThree-value = %d\n",binaryThree);
+  int octalEight=010;
+  printf("octalEight-value = %d\n",octalEight);
+  int hexTen =0xA;
+  printf("hexTen-value = %d\n", hexTen);
+  int asciiValueOfOne =  '1';
+  printf("asciiValueOfOne-value %d\n", asciiValueOfOne);
+  int asciiValueOfA ='A';
+  printf("asciiValueOfA-value = %d\n",asciiValueOfA);
+    
+}
