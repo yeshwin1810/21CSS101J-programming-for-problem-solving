1134d9953135abad99dcf04fee78f714e2b0a040
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 17:09:58 +0530

    mixed data type(arithmetic operators).c
    output:
    RA2211042010009
    Result1 = 4.500000
    Result2 = 75
    result3 = 10.570000

---
+++ chap_79.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+void main() {
+    printf("RA2211042010009\n");
+    printf("Result1 = %f\n",(2+2.5));
+    printf("Result2 = %d\n",('A'+10));
+    printf("result3 = %Lf\n",(5.9 + 4.67L));
+}
