938cb3f62fc87be7dcef6fff7cae4820738e653c
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 15:59:13 +0530

    signed and unsigned values..c
    output:
    RA2211042010009
    given signed values are -20 and 20
    given unsigned values are -1 and 1

---
+++ chap_69.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    signed int number1= -20, number2= 20;
+    unsigned int number3= -1, number4= 1;
+    printf("given signed values are %d and %d\n",number1,number2);
+    printf("given unsigned values are %d and %d\n",number3, number4);
+}
