5e7d1516310ac9f6d4a195223da41b0e16f8eac2
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 16:22:34 +0530

    double data type.c
    output;
    RA2211042010009
    given doubles values are num1=15.479000 and num2= 25.700000
    the result after dividing = 1.660314
    The result after dividing in exponential format = 1.660314e+00

---
+++ chap_73.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    double num1= 15.479, num2= 25.7,result;
+    printf("given doubles values are num1=%f and num2= %f\n",num1, num2);
+    result = num2 / num1;
+    printf("the result after dividing = %f\n",result);
+    printf("The result after dividing in exponential format = %e\n");
+}
