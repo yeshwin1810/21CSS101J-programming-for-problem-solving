c0fd506305cee4d1e32d8ca5bf20f28ad4c489f0
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-02 16:42:25 +0530

    enumerated data type.c
    output:
    RA2211042010009
    The day number stored in d is 4

---
+++ chap_75.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+void main() {
+    printf("RA2211042010009\n");
+    enum day{sunday,monday,tuesday,wednesday,thursday,friday,saturday};
+    enum day d = thursday;
+    printf("The day number stored in d is %d\n",d);
+}
