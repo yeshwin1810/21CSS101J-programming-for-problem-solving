2aadde916f8368cbf6c2f90c6b1f0e938744f6c7
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-06 18:38:13 +0530

    size of operator.c
    output:
    RA2211042010009
    Size of int: 4 bytes
    Size of float: 4 bytes
    Size of double: 8 bytes
    Size of char: 1 bytes

---
+++ chap_93.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    float floattype;
+    double doubletype;
+    printf("Size of int: %zu bytes\n", sizeof(100));
+    printf("Size of float: %zu bytes\n", sizeof(floattype));
+    printf("Size of double: %zu bytes\n",sizeof(doubletype));
+    printf("Size of char: %zu bytes\n",sizeof(char));
+}
