0ecbe4f2cbf29bce6b9c03ca17f9042c5f973855
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-06 19:06:00 +0530

    implict type conversations.c
    output:
    RA2211042010009
    i = 20
    Integer result =117, Float result = 117.000000

---
+++ chap_97.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    int i, intsum;
+    printf("i = ");
+    scanf("%d",&i);
+    char ch = 'a';
+    float floatsum;
+    intsum = ch + i;
+    floatsum = ch + i;
+    printf("Integer result =%d, Float result = %f\n", intsum, floatsum);
+}
