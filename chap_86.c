94a9525a341e997f2398a423f743fc047640d63a
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-06 17:47:22 +0530

    Bitwise operator.c
    output:
    RA2211042010009
    Bitwise AND result = 2

---
+++ chap_86.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010009\n");
+    int num_1 = 10, num_2 = 3, result;
+    result = num_1 & num_2;
+    printf("Bitwise AND result = %d\n", result);
+}
