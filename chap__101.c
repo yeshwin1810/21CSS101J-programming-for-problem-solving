7499facab57902849ae812037976af08e398a954
yeshwin1810 <113873611+yeshwin1810@users.noreply.github.com>
2022-11-24 20:43:17 +0530

    multiplication table.c
    output:
    RA2211042010009
    Enter an integer: 4
    4 * 1 = 4 
    4 * 2 = 8 
    4 * 3 = 12 
    4 * 4 = 16 
    4 * 5 = 20 
    4 * 6 = 24 
    4 * 7 = 28 
    4 * 8 = 32 
    4 * 9 = 36 
    4 * 10 = 40

---
+++ chap__101.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  printf("RA2211042010009\n");
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
